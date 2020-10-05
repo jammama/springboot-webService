a98f275ce92116d4fb72796af45ce01770ea23a5
siznorias <jammama@naver.com>
2020-10-05 13:11:43 +0900

    Initial commit

---
+++ src/main/java/controller/HelloController.java
@@ -0,0 +1,12 @@
+package controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
