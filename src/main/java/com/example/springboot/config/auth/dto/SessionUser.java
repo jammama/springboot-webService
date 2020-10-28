8683b46639be42a55d929aa0b5a8e546553e10cc
siznorias <jammama@naver.com>
2020-10-28 10:28:59 +0900

    'wip'

--- src/main/java/com/example/springboot/config/auth/dto/SessionUser.java
+++ src/main/java/com/example/springboot/config/auth/dto/SessionUser.java
@@ -3,8 +3,10 @@ package com.example.springboot.config.auth.dto;
 import com.example.springboot.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
     private String name;
     private String email;
     private String picture;
